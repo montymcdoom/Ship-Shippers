ef83ca2a880b2a3ff8b7f538e182004b591602db
Ian Montgomery <ianarthurmontgomery@gmail.com>
2021-03-23 17:54:46 +0000

    Oscillator Script commented

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,12 +18,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period;
+        float cycles = Time.time / period; // continually growing over time
         
-        const float tau = Mathf.PI * 2;
+        const float tau = Mathf.PI * 2; // constant value of 6.283...
         float rawSinWave = Mathf.Sin(cycles * tau);
 
-        movementFactor = (rawSinWave + 1f) / 2f;
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 as it's cleaner
 
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
