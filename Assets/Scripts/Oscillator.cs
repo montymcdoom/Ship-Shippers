75fdfc875cb5e192d84480743b53c59091d829fe
Ian Montgomery <ianarthurmontgomery@gmail.com>
2021-04-19 16:37:21 +0100

    Level 5 Started
    Updated landing pad lights to pulse
    Added NaN protection to Oscillator.cs
    Created Level 5 - Moving Landing

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,6 +18,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period; // continually growing over time
         
         const float tau = Mathf.PI * 2; // constant value of 6.283...
